24ae22f241f04053e340a91ace43595c36e2aaba
Zane Fink <finkzane@gmail.com>
2024-06-21 06:57:55 -0700

    updates

--- software_env/install.sh
+++ software_env/install.sh
@@ -2,7 +2,7 @@
 git clone -c feature.manyFiles=true https://github.com/spack/spack.git
 export PATH="$PWD/spack/bin:$PATH"
 spack env create hpacml ./spack.yaml
-. /scratch/bcbs/zanef2/hpacml_artifact/software_env/spack/share/spack/setup-env.sh
+. $PWD/spack/share/spack/setup-env.sh
 spack env activate hpacml -p
 spack external find --all --not-buildable --exclude openssl --exclude openblas --exclude bzip2
 spack concretize
