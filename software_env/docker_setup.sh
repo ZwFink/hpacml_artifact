6ac32b3db1e9c2dcdea8e4f675fccf3330b8acc6
Zane Fink <finkzane@gmail.com>
2024-06-28 15:46:44 -0500

    docker setup script

--- software_env/docker_setup.sh
+++ software_env/docker_setup.sh
@@ -16,6 +16,10 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
   && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
@@ -29,6 +33,9 @@ sudo apt-get install -y nvidia-container-toolkit
 
 sudo nvidia-ctk runtime configure --runtime=docker
 sudo systemctl restart docker
+nvidia-ctk runtime configure --runtime=docker --config=$HOME/.config/docker/daemon.json
+systemctl --user restart docker
+sudo nvidia-ctk config --set nvidia-container-cli.no-cgroups --in-place
 
 echo "Building HPAC-ML Docker environment using 3 cores. You may want to edit the Dockerfile to use all available cores on your machine..."
 sudo docker build -t hpacml .
